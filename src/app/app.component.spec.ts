7294deb8fbb7cf7a6ee3f665b2da4a356e2c4827
pinak007 <pinak007@gmail.com>
2019-08-28 23:28:24 +0530

    changed the test message to app in all files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'travistest'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('travistest');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to travistest!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   });
 });
